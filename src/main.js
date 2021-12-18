c6062bf5da041faf98217b4c9f0bc18cb49857d6
tranchikhang <tranchikhang@outlook.com>
2021-12-18 23:28:11 +0900

    update mainpage

--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ResultList from './components/ResultList.vue'
+import VueRouter from 'vue-router'
+import '../node_modules/Bulma/CSS/bulma.css';
+import './assets/main.css'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/',
+    component: App
+}, {
+    path: '/result',
+    component: ResultList
+}];
+
+const router = new VueRouter({
+    routes: routes
+});
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    el: '#app',
+    router,
+    data: {
+        currentRoute: window.location.pathname
+    },
+});
